49cddf204329f37c9a849763b90e322b8b818e5a
elijah <eyblefap@gmail.com>
2025-12-04 11:04:03 +0300

    3.12.25

--- cpp/labs/prog/3.cpp
+++ cpp/labs/prog/3.cpp
@@ -3,40 +3,49 @@
 #include <iostream>
 using namespace std;
 
-int getRandomInt() { return -50 + rand() % (101); }
-int countPosUneven(int *&arr, int length);
+void createArr(int *&arr, int length);
+int getRandomInt(int start = 0, int end = 100) {
+    return start + rand() % (end + 1);
+}
+int countPositiveUneven(int *&arr, int length);
 void outputArray(int *&arr, int length);
 
 int main(int argc, char *argv[]) {
     srand(time(nullptr));
+    cout << "Задание (a) :" << endl;
     int length;
     cout << "Введите длину массивов n: ";
     cin >> length;
-    int *X = new int[length];
-    int *Y = new int[length];
-    for (int i = 0; i < length; i++) {
-        X[i] = getRandomInt();
-        Y[i] = getRandomInt();
-    }
+    int *X;
+    int *Y;
+    createArr(X, length);
+    createArr(Y, length);
     cout << "Массив X: ";
     outputArray(X, length);
     cout << "Массив Y: ";
     outputArray(Y, length);
     cout << "Кол-во нечетных элементов в массиве X: "
-         << countPosUneven(X, length) << endl;
-    cout << "Кол-во нечетных элементов в массиве X: "
-         << countPosUneven(X, length) << endl;
+         << countPositiveUneven(X, length) << endl;
+    cout << "Кол-во нечетных элементов в массиве Y: "
+         << countPositiveUneven(Y, length) << endl;
     return 0;
 }
 
-void outputArray(int *&arr, int length) {
+void createArr(int *&arr, int length) {
+    arr = new int[length];
+    for (int i = 0; i < length; i++) {
+        arr[i] = getRandomInt(-100, 100);
+    }
+}
+
+void outputArray(const int *&arr, int length) {
     cout << "[ ";
     for (int i = 0; i < length; i++) {
         cout << arr[i] << ((i != (length - 1)) ? ", " : " ]\n");
     }
 }
 
-int countPosUneven(int *&arr, int length) {
+int countPositiveUneven(const int *&arr, int length) {
     int sum{};
     for (int i = 0; i < length; i++) {
         int item = arr[i];
