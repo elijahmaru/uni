f01f57f941577402622cd120a1407ca34763ff9b
elijah <eyblefap@gmail.com>
2025-12-04 13:46:54 +0300

    4.12.25

--- cpp/labs/arc/test.cpp
+++ cpp/labs/arc/test.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <ostream>
 using namespace std;
+int inc(const int *arr) {
+    cout << arr[0] << endl;
+    return arr[0];
+}
 int main(int argc, char *argv[]) {
-    int i{0};
-    cout << (i++) << endl;
+    int *i = new int[4];
+    inc(i);
 }
