48b9fdf4cb61ee02a64ca99182bce7654d4ef570
elijah <eyblefap@gmail.com>
2026-02-12 23:28:20 +0300

    change names of folders for ai labs

---
+++ python/ai/lab3/main.py
@@ -0,0 +1,12 @@
+def mean(data):
+    sum = 0
+    count = 0
+    for i in data:
+        sum += i
+        count += 1
+    return sum/count
+
+def median(data):
+    min = min(data)
+    max = min(data)
+    median
