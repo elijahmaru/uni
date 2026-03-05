53c72c3317761778791b0a42fe3d3ed6d6a2b760
elijah <eyblefap@gmail.com>
2026-03-05 23:50:19 +0300

    ai lab4 finished

--- python/ai/lab4/main.py
+++ python/ai/lab4/main.py
@@ -3,13 +3,13 @@ import numpy as np
 
 # task 1
 data = np.random.rand(24 * 60, 12)
+print(f"Исходная форма: {data.shape}")
 dataReshaped = data.reshape(24, 60, 12)
-print(data.shape)
 if data[0][0] == dataReshaped[0][0][0] and data[60][0] == dataReshaped[1][0][0]:
     data = dataReshaped
 else:
     raise TypeError("Reshape wasn't done properly")
-print(data.shape)
+print(f"Изменённая форма: {data.shape}")
 avgPerHour = data.mean(axis=(1, 2))
 avgPerSensor = data.mean(axis=(0, 1))
 avgPerHourAndSensor = data.mean(axis=1)
@@ -26,23 +26,48 @@ print("Максимальное изменение: ", maxChange)
 np.random.seed(0)
 X = np.random.rand(500, 5)
 centerOfMass = X.mean(axis=0)
-xCenterd = X - centerOfMass
-if np.allclose(xCenterd.mean(axis=0), 0):
+xCentered = X - centerOfMass
+if np.allclose(xCentered.mean(axis=0), 0):
+    X = xCentered
     print("Массив центрирован")
 else:
     raise ValueError("Array is not centered properly")
 startTime = time.perf_counter()
 diff = X[:, None, :] - X[None, :, :]
-d1 = np.sqrt(np.sum((diff * 2), axis=2))
+d1 = np.sqrt(np.sum((diff**2), axis=2))
 endTime = time.perf_counter()
-print(d1.shape)
+print(d1)
 timeForD1 = endTime - startTime
 startTime = time.perf_counter()
 norms = np.sum(X**2, axis=1)
-d2 = np.sqrt(norms[:, None] + norms[None, :] - 2 * X @ X.T)
+d2 = np.sqrt(np.maximum(norms[:, None] + norms[None, :] - 2 * X @ X.T, 0))
 endTime = time.perf_counter()
 timeForD2 = endTime - startTime
 print(f"Время для первого: {timeForD1}\nВремя для второго: {timeForD2}")
 indexOfClothestPoint = np.argmin(d1)
 
 # task 3
+X = np.random.rand(1000, 10)
+print(f"Исходная форма: {X.shape}")
+euclideanNorm = np.sqrt(np.sum(np.square(X), axis=1))
+print(f"{euclideanNorm.shape}")
+X = X / euclideanNorm[:, None]
+euclideanNorm = np.linalg.norm(X, axis=1)
+print(f"Проверка нормалей{np.allclose(euclideanNorm, 1)}")
+
+print()
+
+X = np.random.rand(500, 20)
+startShape = X.shape
+avgPerRow = X.mean(axis=1)
+print(f"Форма среднего по строке{avgPerRow.shape}")
+avgPerRow = avgPerRow[:, None]
+avgPerCol = X.mean(axis=0)
+print(f"Форма среднего по столбцу{avgPerCol.shape}")
+avgPerCol = avgPerCol[None, :]
+boolMask = (X > avgPerRow) & (X < avgPerCol)
+X[boolMask] = 0
+endShape = X.shape
+replacementCount = np.sum(boolMask)
+print(f"Элементов измененно: {replacementCount}")
+print(f"Провекрка неизменности формы :{startShape == endShape}")
