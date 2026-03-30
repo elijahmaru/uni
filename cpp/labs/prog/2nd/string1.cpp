ce2a279bbf9c51ddd9d5b06898e9c17f414ba627
elijah <eyblefap@gmail.com>
2026-03-30 03:36:06 +0300

    changed how string lab for prog was solved

--- cpp/labs/prog/2nd/string1.cpp
+++ cpp/labs/prog/2nd/string1.cpp
@@ -1,4 +1,7 @@
+#include <cctype>
+#include <cstddef>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -31,9 +34,6 @@ int main(int argc, char *argv[]) {
 }
 
 int findWord(string text) {
-    text = " " + text + " ";
-    int amount = 0;
-    size_t i = 0;
     for (vector<string>::iterator it = punctuation.begin();
          it != punctuation.end(); ++it) {
         while (text.find(*it) != string::npos) {
@@ -41,54 +41,79 @@ int findWord(string text) {
             text = text.substr(0, deleteChar) + text.substr(deleteChar + 1);
         }
     }
-    cout << text << endl;
-    while (i < 10) {
-        int startOfTheWord = (text.find(" м", i) < text.find(" М", i))
-                                 ? text.find(" м", i)
-                                 : text.find(" М", i);
-        if (startOfTheWord == string::npos)
-            return amount;
-        int endOfTheWord = text.find("а ", endOfTheWord);
-        if (endOfTheWord == string::npos)
-            return amount;
-        cout << text.substr(startOfTheWord, endOfTheWord - startOfTheWord)
-             << "end" << endl;
-        i = endOfTheWord;
+    if (text.length() == 0) {
+        return 0;
+    }
+    for (int pos = 0; pos < text.length() && text.at(pos) == ' ';) {
+        text.erase(0, 1);
+    }
+    for (int pos = text.length() - 1; pos >= 0 && text.at(pos) == ' '; pos--) {
+        text.erase(pos, 1);
+    }
+    int amount = 0;
+    string word;
+    stringstream ssText(text);
+    while (ssText >> word) {
+        if (word.length() >= 2) {
+            string fst = word.substr(0, 1);
+            string last = word.substr(word.length() - 1, 1);
+            if ((fst == "м" || fst == "М") && last == "а") {
+                amount++;
+                cout << word << endl;
+            }
+        }
     }
     return amount;
 }
 
 int indexOfMax(const vector<int> &vec) {
     int index = 0;
-    for (size_t i = 0; i < vec.size(); i++) {
-        if (vec.at(index) < vec.at(i)) {
-            index = i;
+    for (size_t pos = 0; pos < vec.size(); pos++) {
+        if (vec.at(index) < vec.at(pos)) {
+            index = pos;
         }
     }
     return index;
 }
 
 string findMostStartingChar(string text) {
-    if (text.at(text.length() - 1) == ' ') {
-        text = text.substr(0, text.length() - 1);
-    }
-    vector<int> times;
-    string startChars;
-    times.push_back(1);
-    startChars += text.at(0);
-    for (size_t i = 1; i < text.length() && text.find(" ", i) != string::npos;
-         i++) {
-        size_t pos = text.find(" ", i);
-        if (pos == string::npos || pos + 1 >= text.length())
-            break;
-
-        char potStart = text.at(pos + 1);
-        if (startChars.find(potStart) != string::npos) {
-            times.at(startChars.find(potStart))++;
-        } else {
-            times.push_back(1);
-            startChars += potStart;
+    for (vector<string>::iterator it = punctuation.begin();
+         it != punctuation.end(); ++it) {
+        while (text.find(*it) != string::npos) {
+            int deleteChar = text.find(*it);
+            text = text.substr(0, deleteChar) + text.substr(deleteChar + 1);
         }
     }
-    return startChars.substr(indexOfMax(times), 1);
+    if (text.length() == 0) {
+        return "";
+    }
+    for (int pos = 0; pos < text.length() && text.at(pos) == ' ';) {
+        text.erase(0, 1);
+    }
+    if (text.length() == 0) {
+        return "";
+    }
+    for (int pos = text.length() - 1; pos >= 0 && text.at(pos) == ' '; pos--) {
+        text.erase(pos, 1);
+    }
+    if (text.length() == 0) {
+        return "";
+    }
+    for (size_t pos = 0; pos < text.length(); pos++) {
+        text[pos] = tolower(text.at(pos));
+    }
+    vector<int> howMany;
+    string startChar;
+    string word;
+    stringstream ssText(text);
+    while (ssText >> word) {
+        size_t pos = startChar.find(word.substr(0, 1));
+        if (pos == string::npos) {
+            startChar += word.substr(0, 1);
+            howMany.push_back(1);
+        } else {
+            howMany[pos]++;
+        }
+    }
+    return startChar.substr(indexOfMax(howMany), 1);
 }
