981fbaee76c77940eddc93ffcb2e1eef4e545554
elijah <eyblefap@gmail.com>
2026-04-24 11:54:32 +0300

    ai lab6 task 2 finished

--- python/ai/lab6/main.py
+++ python/ai/lab6/main.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import numpy as np
 
+# task 1
 initialReport = {}
 dataSet = pd.read_csv("dataSet.csv")
+print("File loaded")
 print(dataSet)
 print("\n" * 5)
 print("INFO:")
@@ -17,11 +20,32 @@ print("\n" * 5)
 print("TYPES:")
 initialReport["dtypes"] = dataSet.dtypes
 print(initialReport["dtypes"])
-
-amountOfNaN = dataSet.isna().sum()
+amountOfNaN = dataSet.isna().sum().sum()
 amountOfDublicates = dataSet.duplicated(keep=False).sum()
 amountOfUnique = dataSet.nunique()
 top5 = dataSet.select_dtypes(include=["string", "object"]).value_counts().head()
 print("INITIAL REPORT")
 initialReport.update({"missing": amountOfNaN, "duplicates": amountOfDublicates})
 print(initialReport)
+
+# task 2
+dataSetClean = dataSet.copy()
+dataSetClean.fillna(dataSetClean.median(numeric_only=True), inplace=True)
+afterNumFill = amountOfNaN - dataSetClean.isna().sum().sum()
+amountOfNaN = dataSetClean.isna().sum().sum()
+datesCol = ["time", "year", "month", "day_of_year", "hour"]
+dataSetClean["time"].replace("", np.nan, inplace=True)
+dataSetClean.dropna(subset=["time"], inplace=True)
+afterTimeDelete = amountOfNaN - dataSetClean.isna().sum().sum()
+amountOfNaN = dataSetClean.isna().sum().sum()
+dataSetClean.fillna(
+    dataSetClean.mode().iloc[0], inplace=True
+)  # заполнеят все пустые места модой
+afterStringFill = amountOfNaN - dataSetClean.isna().sum().sum()
+amountOfNaN = dataSetClean.isna().sum().sum()
+dataSetClean.drop_duplicates(keep="first")
+dataSetClean.reset_index(drop=True, inplace=True)
+print("INFO:\nDataSet before cleaning:\n", dataSet.info())
+print("DataSet after cleaning:\n", dataSetClean.info())
+
+# task 3
