75555e39cd20ab1948dbca5072d4b81645fad46f
elijah <eyblefap@gmail.com>
2026-05-08 12:50:35 +0300

    ai lab6 finished

--- python/ai/lab6/main.py
+++ python/ai/lab6/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # task 1
 initialReport = {}
@@ -34,7 +35,7 @@ dataSetClean.fillna(dataSetClean.median(numeric_only=True), inplace=True)
 afterNumFill = amountOfNaN - dataSetClean.isna().sum().sum()
 amountOfNaN = dataSetClean.isna().sum().sum()
 datesCol = ["time", "year", "month", "day_of_year", "hour"]
-dataSetClean["time"].replace("", np.nan, inplace=True)
+dataSetClean[datesCol] = dataSetClean[datesCol].replace("", np.nan)
 dataSetClean.dropna(subset=["time"], inplace=True)
 afterTimeDelete = amountOfNaN - dataSetClean.isna().sum().sum()
 amountOfNaN = dataSetClean.isna().sum().sum()
@@ -45,7 +46,41 @@ afterStringFill = amountOfNaN - dataSetClean.isna().sum().sum()
 amountOfNaN = dataSetClean.isna().sum().sum()
 dataSetClean.drop_duplicates(keep="first")
 dataSetClean.reset_index(drop=True, inplace=True)
-print("INFO:\nDataSet before cleaning:\n", dataSet.info())
-print("DataSet after cleaning:\n", dataSetClean.info())
+print("INFO:\nDataSet before cleaning:\n")
+dataSet.info()
+print("DataSet after cleaning:\n")
+dataSetClean.info()
 
 # task 3
+dataSetClean["isHumanMade"] = (
+    True if "explosion" in dataSetClean["type"].str.lower() else False
+)
+# создайт новую колонку если землетресение было вызванно взрывом
+dataSetClean["energyEstimate"] = 10 ** (1.5 * dataSetClean["mag"] + 4.8)
+# колво энергие выделенно
+dataSetClean["part_of_day"] = pd.cut(
+    dataSetClean["hour"],
+    bins=[0, 6, 12, 18, 24],
+    labels=["Night", "Morning", "Afternoon", "Evening"],
+    right=False,
+)
+# какая часть дня
+
+# task4
+
+dataSetClean.describe()
+top10 = (
+    dataSetClean.groupby("place")["energyEstimate"].sum().sort_values(ascending=False)
+)
+print("Finished sorting")
+plt.figure(figsize=(12, 7))
+top10.plot(kind="barh")
+
+plt.xlabel("Total Energy")
+plt.ylabel("Place")
+plt.title("Top10 Places by Total Earthquake Energy")
+
+# plt.gca().invert_yaxis()
+
+plt.show()
+
